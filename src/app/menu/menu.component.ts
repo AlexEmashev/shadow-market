1aa46d074ab4a170f6ddc1a51e36653c26f89540
Alexander Emashev <alex.emashev@gmail.com>
2018-04-02 12:15:07 +0300

    Add menu toggle depending on the screen width.

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit, AfterViewInit, ViewChild } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ViewChild, HostListener } from '@angular/core';
 import { ToolbarService } from '../toolbar/toolbar.service';
 import { MatSidenav } from '@angular/material';
+
 
 @Component({
   selector: 'app-menu',
@@ -9,20 +10,34 @@ import { MatSidenav } from '@angular/material';
 })
 export class MenuComponent implements OnInit, AfterViewInit {
   /**
+   * Breakpoint for Sidenav displaying mode.
+   */
+  menuBreakpoing = 720;
+
+  /**
   * Get access to sidenav component on view.
   */
   @ViewChild('mainSidenav')
   mainSidenav:MatSidenav;
+
+  /**
+   * Add event listenet on window resize.
+   */
+  @HostListener('window:resize') onResize() {
+    this.switchSidenavMode();
+  }
+
 
   constructor(private toolbar: ToolbarService) { }
 
   ngOnInit() {
     // Subscribe to the observable and connect it to a message.
     this.toolbar.currentMessage.subscribe(message => this.onToolbarStateChanged(message));
+    this.switchSidenavMode();
   }
 
   /**
-  * Get access to component view.
+  * Get access to a component view.
   */
   ngAfterViewInit() {
   }
@@ -38,4 +53,18 @@ export class MenuComponent implements OnInit, AfterViewInit {
     }
   }
 
+  /**
+   * Switches Sidenav mode depending on the screen width.
+   */
+  switchSidenavMode() {
+    let width = window.innerWidth;
+
+    if (width > this.menuBreakpoing) {
+      this.mainSidenav.mode = 'side';
+      this.mainSidenav.open();
+    } else {
+      this.mainSidenav.mode = 'over';
+      this.mainSidenav.close();
+    }
+  }
 }
