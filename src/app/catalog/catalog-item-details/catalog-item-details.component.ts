031844f4aa9dc3494127038331fd15eb71c2f7cd
Alexander Emashev <alex.emashev@gmail.com>
2018-04-20 18:26:44 +0300

    Item details component created

--- src/app/catalog/catalog-item-details/catalog-item-details.component.ts
+++ src/app/catalog/catalog-item-details/catalog-item-details.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { CatalogItem } from '../catalog-item';
+import { CatalogService } from '../catalog.service';
+import { Location } from '@angular/common';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Observable } from 'rxjs/Observable';
+import { UserSettingsService } from '../../user-settings.service';
 
 @Component({
   selector: 'app-catalog-item-details',
@@ -6,10 +12,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./catalog-item-details.component.scss']
 })
 export class CatalogItemDetailsComponent implements OnInit {
+  /**
+   * Catalog item.
+   */
+  item: CatalogItem;
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private router: Router,
+    private location: Location,
+    private userSettings: UserSettingsService,
+    private catalogService: CatalogService
+  ) {
+    this.item = {
+      id: null,
+      user_id: null,
+      title: null,
+      description: null,
+      photos: [],
+      price: null,
+      likes: 0,
+      views: 0
+    };
+  }
 
   ngOnInit() {
+    // Get item using item id.
+    const itemId = +this.route.snapshot.paramMap.get('id')
+    this.catalogService.getItem(itemId)
+      .subscribe((item) => this.item = item);
   }
 
 }
