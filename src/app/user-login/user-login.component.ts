b8d30a02ef8e20b8385b0cd178f0ad91ee8712aa
Alexander Emashev <alex.emashev@gmail.com>
2018-05-03 02:18:32 +0300

    Added localization for new items

--- src/app/user-login/user-login.component.ts
+++ src/app/user-login/user-login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
 import { UserSettingsService } from '../user-settings.service';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-user-login',
@@ -11,13 +12,18 @@ export class UserLoginComponent implements OnInit {
   login_str: string
   password_str: string;
   showUserNotFound: boolean;
+  placeholderLogin: string;
+  placeholderPassword: string;
 
   constructor(
     public dialogRef: MatDialogRef<UserLoginComponent>,
     private userSettings: UserSettingsService,
+    private translate: TranslateService,
     @Inject(MAT_DIALOG_DATA) public data: any
   ) {
       this.showUserNotFound = false;
+      translate.get('loginForm.login_input').subscribe((res: string) => this.placeholderLogin = res);
+      translate.get('loginForm.password_input').subscribe((res: string) => this.placeholderPassword = res);
      }
 
   ngOnInit() {
